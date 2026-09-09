c12520f02ddb3f5637802bdeea94609776f694cb
João Pedro <jp413893@gmail.com>
2026-09-09 10:54:20 -0300

    Corrige case do import Menu

--- src/router.jsx
+++ src/router.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import RootLayout from "./pages/RootLayout";
 import Home from "./pages/Home";
-import Menu from "./pages/menu";
+import Menu from "./pages/Menu";
 import Delivery from "./pages/Delivery";
 
 const router = createBrowserRouter([{
